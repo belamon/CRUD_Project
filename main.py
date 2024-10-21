73b23bdc831f7ff4075523eee1dc9e5d9c40c13d
Bela <belamoneta@gmail.com>
2024-10-21 22:23:36 +0700

    Step 4: Create Leads

--- main.py
+++ main.py
@@ -6,7 +6,7 @@
 
 
 # /************************************/
-
+import re
 # /===== Data Model =====/
 # Create your data model here
 data_lead = [
@@ -98,6 +98,144 @@ def show_data_leads():
         except ValueError:
             print("Invalid input. Please enter a number.") #inform the user that the input should be a number
 
+#Create menu option 
+def create_data_lead():
+    """Function to display the result when the user choosing number 2 on main menu """
+    while True:
+        # Displaying the main menu for creatin a new lead or going back 
+        print("\t\t=========CREATE NEW LEAD===============")
+        print("1. Create New Lead")
+        print("2. Back to Menu")
+
+        try:  # This is a block to handle any invalid input like non-integer values 
+            user_input = int(input("Please enter 1 to create a lead or 2 to go back:"))
+
+            if user_input == 1:
+                print("===========Input Lead Details===========")
+
+                while True:
+                    # Asking user for an email and validate the format
+                    while True:
+                        lead_email = input("Enter Lead's Email: ") 
+                        if validate_email(lead_email):
+                            break
+                        else:
+                            print("Invalid Email Format. Please try again.")
+
+                    # Checking if the email already exists 
+                    lead_exists = lead_email in (lead['email'] for lead in data_lead)  # this code is for checking if the email already exists in the list
+
+                    if lead_exists:
+                        # If the email already exists, inform the user and display the email
+                        print("\t\t=====LEAD DATA CHECK========")
+                        print("This email already exists in the database")
+                        break
+
+                    #inner loop for checking lead_id
+                    while True:
+                        try:
+                            lead_id = int(input("Enter Lead ID: "))
+                            id_exists = lead_id in (lead['lead_id'] for lead in data_lead)
+                            if id_exists:
+                                print("Lead ID already exists. Please input another Lead ID")
+                            else:
+                                break #exit the loop if a unique lead is provided
+                        except ValueError:  # Handle the case where conversion to int fails
+                            print("Invalid input. Please input a number.")
+                                      
+
+                    # Collect lead details from the user 
+                    first_name = input("Enter First Name of the Lead: ")
+                    last_name = input("Enter Last Name of the Lead: ")
+
+                    # Prompt for phone number and validate it 
+                    while True:
+                        phone_number = input("Enter Phone Number: ")
+                        if validate_phone_number(phone_number):
+                            break
+                    #Prompt company sector and validate it 
+                    
+                    while True:
+                        company_sector = input("Enter Company Sector of the Lead: ")
+                        if validate_sector(company_sector):
+                            break
+                    #Prompt lead source and validate it 
+                    while True:
+                        lead_source = input("Enter Lead Source of the Lead: ")
+                        if validate_source(lead_source):
+                            break
+                    #Validate the date input    
+                    while True:
+                        date_created = input("Enter Date Created of the Lead: ")
+                        if validate_date(date_created):
+                            break
+                    #Validate sales representation 
+                    while True:
+                        sales_rep = input("Enter Sales Representative:")
+                        if validate_sales(sales_rep):
+                            break
+
+                    while True:
+                        try:
+                            transaction = int(input("Enter the new Transaction: "))
+                            print(f"Transaction updated to: {transaction}")
+                            break  # Exit the loop if a valid integer is provided
+                        except ValueError:
+                            print("Please input a valid number.")  # Inform the user to input a number
+
+
+                    # Store the lead details in a dictionary 
+                    new_lead = {
+                        "lead_id": lead_id,
+                        "first_name": first_name, 
+                        "last_name": last_name,
+                        "email": lead_email,
+                        "phone_number": phone_number,
+                        "company_sector": company_sector,
+                        "lead_source": lead_source,
+                        "date_created": date_created,
+                        "sales_rep":sales_rep,
+                        "Transaction":transaction
+                    }
+
+                    # Display a confirmation page
+                    print("\n=== Confirmation Page ===")
+                    print(f"First Name: {first_name}")
+                    print(f"Last Name: {last_name}")
+                    print(f"Email: {lead_email}")
+                    print(f"Phone Number: {phone_number}")
+                    print(f"Company Sector: {company_sector}")
+                    print(f"Lead Source: {lead_source}")
+                    print(f"Date Created: {date_created}")
+                    print(f"Sales Representation: {sales_rep}")
+                    print(f"Transaction: {transaction}")
+                        
+
+                    #Ask for confirmation until a valid input is received 
+                    confirm = confirmation_page(action="Save", data=new_lead)
+                    if confirm == "1":
+                        print("\nLead Added Successfully!")
+                        data_lead.append(new_lead)
+                        break
+                    elif confirm == "2":
+                        print("\nLead not added.")
+                        break
+                    else:
+                        print("Invalid input. Please choose between 1 or 2.")
+
+
+            elif user_input == 2:
+                # If user selects 2, then they will return to the main menu 
+                print("Returning to the main menu ")
+                break  # Exit the loop and back to the main menu 
+
+            else:
+                # If user enters a number other than 1 or 2, show an error message 
+                print("Invalid Input. Please enter 1 or 2")
+
+
+        except ValueError:
+            print("Invalid Input. Please enter the number between (1-2)")
 
 
 def update():
@@ -160,6 +298,103 @@ def details_of_leads(data_lead): #dont forget put the parameneter here to call t
         print(f"Sales Representation = {detail['sales_rep']}")
         print(f"Transaction = {detail['Transaction']}")
 
+def validate_email(email):
+    """
+    Function to validate if an email is in the correct format.
+
+    Returns True if valid, False otherwise
+    """
+    # Regular expression for validating an email address
+    email_regex = r'^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$'
+
+    #user re.match() to check if the email matches the regex pattern 
+    if re.match(email_regex, email):
+        return True
+    else:
+        return False
+    
+def validate_phone_number(phone_number):
+    #this is regex pattern to allow numbers, spaces, +, _, and parentheses
+    pattern = r'^\+?[0-9\s\-()]{7,15}$'
+
+    #validate phone number
+    if re.match(pattern, phone_number):
+        return True
+    else:
+        print("Invalid phone number. Please enter a 10-digit number, e.g., 123-456-7890.")
+        return False
+def validate_sector(company_sector):
+    if company_sector.lower() in [sector.lower() for sector in valid_sectors]:
+        return True
+    else:
+        print("Company sector is not listed")
+        print("Here are the available company sectors you can search:")
+        print("Finance")
+        print("Technology")
+        print("Real Estate")
+        return False
+def validate_source(lead_source):
+    if lead_source.lower() in [source.lower() for source in valid_lead_sources]:
+        return True
+    else:
+        print("Lead source is not listed")
+        return False
+def validate_date(date_created):
+    """This is a function to validate if an input date is in the correct format
+
+    Returns True if valid, False otherwise
+    """
+    try:
+        #try to parse the input date using the format 'yyyy/mm/dd'
+        datetime.strptime(date_created, "%Y/%m/%d")
+        return True
+    except ValueError:
+        print("Invalid date. Please enter a date in the format 'yyyy/mm/dd'.")
+        return False
+def validate_sales(sales_rep):
+    if sales_rep.lower() in [rep.lower() for rep in valid_sales_reps]:
+        return True
+    else:
+        print("Sales representation is not listed")
+        return False
+    
+def confirmation_page(action, data):
+    """Generates a confirmation message based on the specified action and data.
+    
+
+    Args:
+        action (str): The action being confirmed, either 'delete' or 'update'
+        -'delete' to confirm a delete operation 
+        -'update' to confirm an update operation 
+
+        data (str): A string representing the specific data (e.g. a record, ID, item)
+
+        Return: str: a formatted strin asking for confrmation to either delete or update the provided data. 
+        if the action is neither delete or update. it returns to none
+
+        example:
+        confirmation_page('delete', 'lead id 789')
+        #returns: are you sure you wan to delete the following data:?
+
+        example:
+        confirmation_page('update', 'lead id 789')
+        #returns: are you sure you wan to update the following data:?
+
+
+    """
+    while True:
+        print(f"Do you want to {action} this lead?")
+        print("1. Yes")
+        print("2. No")
+
+        user_input = input("Enter your choice (1-2):")
+
+        if user_input in ["1", "2"]:
+            return user_input
+        else:
+            print("Invalid choice. Please enter 1 for Yes or 2 for No")
+
+
 # /===== Main Program =====/
 # Create your main program here
 # Main program loop
@@ -170,6 +405,8 @@ while True:
             menu_choice = main_menu_choice()
             if menu_choice == 1:
                 show_data_leads()
+            elif menu_choice == 2:
+                create_data_lead()  # Ensure this function is defined
 
         
     else:
