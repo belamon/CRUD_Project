11186d0fd0f1ec0b3fbd1973469949e89e29f8b7
Bela <belamoneta@gmail.com>
2024-10-21 21:58:20 +0700

    Step 2: Main Menu

--- main.py
+++ main.py
@@ -20,6 +20,30 @@ valid_user = [
 
 # /===== Feature Program =====/
 # Create your feature program here
+
+#Main Menu Function 
+def main_menu_choice():
+    """Function to display the main menu and get user input"""
+    while True: #This will keep asking usert for input until a valid menu option is seleted
+        print("\n\t================LEAD MANAGEMENT SYSTEM================")
+        print("1. Show Leads")
+        print("2. Create Leads")
+        print("3. Update Leads")
+        print("4. Delete Leads")
+        print("5. Check Transaction")
+        print("6. Export Report")
+        print("7. Exit")
+
+        
+        try:
+            menu = int(input("Input program menu from (1-7): ")) #menu is the variable to hold menu value 
+            if 1 <= menu <= 7:  # check if input is valid between this range
+                return menu #return the selected menu option 
+            else:
+                print("*************** Invalid choice. Please select a number between 1 and 5. ***************")
+        except ValueError:
+            print("*********** Invalid input. Please enter a valid number between 1 and 5. ************")
+            
 def read():
     """Function for read the data
     """
@@ -60,9 +84,11 @@ def login():
 while True:
     # Ensure login before allowing access to the menu
     if login():  # Only proceed if the login is successful
-        print("Login Sucessfully")
-        break
+        while True:
+            menu_choice = main_menu_choice()
+
+            
     else:
         # Failed login, retry
         print("Login failed. Please try again.")
-        break
+        
